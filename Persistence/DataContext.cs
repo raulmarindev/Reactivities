3e2565829f2d9cb459537e74a1f2f16fa4b16f1b
Raúl Marín <rmarin82@gmail.com>
2020-02-11 11:36:00 +0100

    Add Activity domain model and seed data for it.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,8 @@ namespace Persistence
 
         public DbSet<Value> Values { get; set; }
 
+        public DbSet<Activity> Activities { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Value>()
