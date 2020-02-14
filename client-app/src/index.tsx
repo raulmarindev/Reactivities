277e4ab6f56602379f0802cdbc91ce2c5902ec25
Raúl Marín <rmarin82@gmail.com>
2020-02-14 19:48:15 +0100

    Move different files and create an organized folder structure.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
+import App from './app/layout/App';
+import './app/layout/styles.css';
+import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
