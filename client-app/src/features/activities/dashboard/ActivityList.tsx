bbe5bb2034b3c2ef6a8399c4ee64e9991a3b72ef
Raúl Marín <rmarin82@gmail.com>
2020-03-26 20:00:07 +0100

    Remove commented code from the ActivityList component.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -72,29 +72,4 @@ export const ActivityList: React.FC = () => {
             ))}
         </List>
     );
-
-    // return (
-    //     <Segment clearing>
-    //         <Item.Group divided>
-    //             {activities.map(activity => (
-    //                 <Item key={activity.id}>
-    //                     <Item.Content>
-    //                         <Item.Header as='a'>{activity.title}</Item.Header>
-    //                         <Item.Meta>{activity.date}</Item.Meta>
-    //                         <Item.Description>
-    //                             <div>{activity.description}</div>
-    //                             <div>{activity.city}, {activity.venue}</div>
-    //                         </Item.Description>
-    //                         <Item.Extra>
-    //                             <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue' />
-    //                             <Button floated='right' content='Delete' color='red' onClick={() => { dispatch(deleteActivity(activity.id)); }} loading={isSubmitting && activity.id === selectedActivity?.id} />
-    //                             <Label basic content={activity.category} />
-    //                         </Item.Extra>
-    //                     </Item.Content>
-    //                 </Item>
-    //             ))
-    //             }
-    //         </Item.Group>
-    //     </Segment>
-    // );
 };
