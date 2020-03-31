521d5dcd2f038a4c861a0ce2db6b97c1752ffc1d
Raúl Marín <rmarin82@gmail.com>
2020-03-31 19:58:54 +0200

    Move the fetching of the activities to the activity dashboard component.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,8 @@
 import 'app/layout/App.css';
-import { LoadingComponent } from 'app/layout/LoadingComponent';
 import { NavBar } from 'features/nav/NavBar';
 import { Container, makeStyles } from 'material';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { RenderRoute, Routes } from 'routes';
-import { AppDispatch, useTypedSelector } from 'store';
-import { fetchActivities, } from 'store/activityDashboard';
+import React from 'react';
+import { RenderRoute, Routes, routesKeys } from 'routes';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,25 +12,18 @@ const useStyles = makeStyles(theme => ({
 const App: React.FC = () => {
   const classes = useStyles();
 
-  const loading = useTypedSelector<boolean>(({ activityDashboardReducer }) => activityDashboardReducer.isFetching);
-
-  const dispatch = useDispatch<AppDispatch>();
-
-  useEffect(() => {
-    dispatch(fetchActivities());
-  }, [dispatch]);
-
-  if (loading) return (<LoadingComponent content="Loading activities..." />);
-
   return (
     <>
-      <NavBar>
-        <Container className={classes.root}>
-          {
-            Array.from(Routes).map(r => <RenderRoute key={r[0]} {...r[1]} />)
-          }
-        </Container>
-      </NavBar>
+      <RenderRoute {...(Routes.get(routesKeys.Home))!} />
+      <RenderRoute {...(Routes.get(routesKeys.InternalArea))!} render={() =>
+        <NavBar>
+          <Container className={classes.root}>
+            {
+              Array.from(Routes).filter(r => r[0] !== routesKeys.Home).map(r => <RenderRoute key={r[0]} {...r[1]} />)
+            }
+          </Container>
+        </NavBar>
+      } />
     </>
   );
 };
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,8 +1,21 @@
+import { LoadingComponent } from 'app/layout/LoadingComponent';
 import { ActivityList } from 'features/activities/dashboard/ActivityList';
 import { Grid } from 'material';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch, useTypedSelector } from 'store';
+import { fetchActivities } from 'store/activityDashboard';
 
 export const ActivityDashboard: React.FC = () => {
+    const loading = useTypedSelector<boolean>(({ activityDashboardReducer }) => activityDashboardReducer.isFetching);
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        dispatch(fetchActivities());
+    }, [dispatch]);
+
+    if (loading) return (<LoadingComponent content="Loading activities..." />);
     return (
         <Grid container spacing={3}>
             <Grid item xs={8}>
